1a3ec2ab35e6f27ffac1ffdb00ab155dc10878fe
user <stel-vd@yandex.ru>
2022-07-12 14:31:10 +0300

    First commit with test

---
+++ src/test/java/com/byndyusoft/ContatsTest.java
@@ -0,0 +1,59 @@
+package com.byndyusoft;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.LocatorsPage;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class ContatsTest {
+
+    @Tag("smokeTest")
+
+    @BeforeEach
+    public void beforeAll() {
+        Configuration.baseUrl = "https://www.google.ru/";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    LocatorsPage locatorsPage = new LocatorsPage();
+
+    @Test
+    @DisplayName("Автоматизация скрипта проверки контактной информации на сайте компании Byndyusoft")
+    void byndyusoftContactsTest() {
+        step("Откройте сайт https://www.google.ru/", () -> {
+            open(baseUrl);
+        });
+
+        step("В поисковой строке введите Byndyusoft", () -> {
+
+            $("[name=q]").click();
+            $(".gLFyf").setValue("Byndyusoft").pressEnter();
+        });
+
+        step("Перейдите по первой ссылке", () -> {
+            $$(".g .LC20lb").first().click();
+        });
+
+        step("Нажмите на желтую кнопку \"Заказать презентацию\" в разделе \"" +
+                "Узнайте больше о наших продуктах\"", () -> {
+            Selenide.switchTo().window(1);
+            $(".btn.btn--lg.btn--info").scrollIntoView(true).click();
+        });
+
+        step("Проверьте корректность номера телефона и почтового ящика", () -> {
+            $x("//a[@href='tel:88007751521']").shouldHave(text("8 800 775-15-21"));
+            $x("//a[contains(text(),'sales@byndyusoft.com')]").shouldBe(visible);
+        });
+    }
+}
+
+
---
+++ src/test/java/pages/LocatorsPage.java
@@ -0,0 +1,19 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class LocatorsPage {
+    //locators
+    SelenideElement search = $("[name=q]"),
+            searchInput = $(".gLFyf"),
+            firstLink = $$(".g .LC20lb"),
+            aboutProducts = $(".btn.btn--lg.btn--info"),
+            tel = $x("//a[@href='tel:88007751521']"),
+            email = $x("//a[contains(text(),'sales@byndyusoft.com')]");
+
+    //actions
+
+}
